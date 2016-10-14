e26ba2250077f122e7a4ba6e9261b75a47939598
Jacobo <jquirozo@outlook.com>
2016-10-14 18:41:09 -0500

    promisses
    Se crea servicio para obtener datos y se obtienen sus datos en evento
    inicial de app mediante una promesa

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -64,34 +65,37 @@ import { Hero } from './hero';
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+	`],
+	providers: [HeroService]
 
 })
 
-export class AppComponent { 
-	  title = 'Tour of Heroes';
-	  selectedHero : Hero;
-	  heroes = HEROES;
+export class AppComponent implements OnInit{ 
+	title = 'Tour of Heroes';
+	selectedHero : Hero;
+	heroes: Hero[];
 	
-	  onSelect(hero: Hero): void {
+	constructor(private heroService: HeroService) { }	
+	
+	getHeroes(): void {
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
+	
+	getHeroesSlowly(): void {
+		this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+	}
+	
+	ngOnInit(): void {
+	  this.getHeroes();
+	  // this.getHeroesSlowly():
+	}
+	
+	onSelect(hero: Hero): void {
       this.selectedHero = hero;
     }
 
-
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 
 
