2436c9a3edb95f69b82e049f2237d69287c45f3f
Jacobo <jquirozo@outlook.com>
2016-10-21 11:51:48 -0500

    http
    se agrega funcionalidad para editar, eliminar y agregar heroes via HTTP.
    Se agrega funcionalidad de busqueda

--- app/app-routing.module.js
+++ app/app-routing.module.js
@@ -16,8 +16,8 @@ var hero_detail_component_1 = require('./hero-detail.component');
 var routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: dashboard_component_1.DashboardComponent },
-    { path: 'detail/:id', component: hero_detail_component_1.HeroDetailComponent },
-    { path: 'heroes', component: heroes_component_1.HeroesComponent }
+    { path: 'heroes', component: heroes_component_1.HeroesComponent },
+    { path: 'detail/:id', component: hero_detail_component_1.HeroDetailComponent }
 ];
 var AppRoutingModule = (function () {
     function AppRoutingModule() {
